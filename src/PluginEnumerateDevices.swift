dde29257d89162e3df1a35ac4f6c16f21ce72398
Sandor Pethes <git@pethes.com>
2019-06-11 08:30:41 -0400

    Removing customizations.

--- src/PluginEnumerateDevices.swift
+++ src/PluginEnumerateDevices.swift
@@ -19,8 +19,8 @@ class PluginEnumerateDevices {
 
 		for device: AVCaptureDevice in devices {
 			var facing: String
-            let hasAudio = device.hasMediaType(AVMediaType.audio)
-            let hasVideo = device.hasMediaType(AVMediaType.video)
+			let hasAudio = device.hasMediaType(AVMediaType(rawValue: convertFromAVMediaType(AVMediaType.audio)))
+			let hasVideo = device.hasMediaType(AVMediaType(rawValue: convertFromAVMediaType(AVMediaType.video)))
 
 			switch device.position {
 			case AVCaptureDevice.Position.unspecified:
@@ -49,4 +49,9 @@ class PluginEnumerateDevices {
 
 		callback(json)
 	}
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromAVMediaType(_ input: AVMediaType) -> String {
+	return input.rawValue
 }
